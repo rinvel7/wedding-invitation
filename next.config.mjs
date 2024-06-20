98de6500e2f5e69a203b3eca0e36127d5fdc59ae
rinvel7 <rinavelasquez03@gmail.com>
2024-06-20 19:27:22 -0400

    add  module.css

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+
+
 const nextConfig = {};
 
 export default nextConfig;
+
+const withCSS = require('@zeit/next-css');
+
+module.exports = withCSS({
+  reactStrictMode: true,
+  // otras configuraciones
+});
