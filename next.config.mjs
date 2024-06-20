cae4885c6ace880c6cd0072eb87c44cd3c1f5ea3
rinvel7 <rinavelasquez03@gmail.com>
2024-06-20 19:51:25 -0400

    fix next configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-
-
 const nextConfig = {};
 
 export default nextConfig;
-
-const withCSS = require('@zeit/next-css');
-
-module.exports = withCSS({
-  reactStrictMode: true,
-  // otras configuraciones
-});
